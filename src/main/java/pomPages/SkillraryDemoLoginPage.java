f076423a2c8b2f22a928389b2794f30319e67c7e
Lenovo <Lenovo@CLIENT1>
2022-10-12 12:37:04 +0530

    hi

---
+++ src/main/java/pomPages/SkillraryDemoLoginPage.java
@@ -0,0 +1,26 @@
+package pomPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SkillraryDemoLoginPage {
+	@FindBy(id="course")
+	private WebElement courcetab;
+
+	public WebElement getCourcetab() {
+		return courcetab;
+	}
+
+	@FindBy(xpath="//a[text()='Selenium Training']")
+	private WebElement seleniumtraining;
+	
+	public SkillraryDemoLoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void seleniumtrainingtab() {
+		seleniumtraining.click();
+	}
+}
